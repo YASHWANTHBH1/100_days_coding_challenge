9011594ef3bc323ac4faea5cb43047fc57004d82
YASHWANTHBH1 <yashwanthbh382@gmail.com>
2025-02-26 12:42:42 +0530

    Create majority_element.py

---
+++ Day1/majority_element.py
@@ -0,0 +1,18 @@
+# leetcode: 169. Majority Element
+# https://leetcode.com/problems/majority-element/
+class Solution(object):
+    def majorityElement(self, nums):
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:#if the count is zero then the candidate is the current number
+                candidate = num#assign the current number to the candidate
+            count += (1 if num == candidate else -1)#if the current number is the candidate then increment the count else decrement the count
+        return candidate#return the candidate
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+
+# Creating an instance of Solution class
+solution = Solution()
+
+# Calling majorityElement function and printing result
+print("Majority Element:", solution.majorityElement(nums))
